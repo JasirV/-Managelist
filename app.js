a9f26c7b2818141d0c659f3f24d4db25294f9c13
JasirV <jasirmon133@gmail.com>
2024-02-19 10:56:21 +0530

    last commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const user=require('express')();
+const { json } = require('express');
 const fs =require('fs')
 const PORT=3000
 user.listen(PORT,()=>console.log(`Server Run ON ${PORT}`))
@@ -6,7 +7,13 @@ user.listen(PORT,()=>console.log(`Server Run ON ${PORT}`))
 
 let data=JSON.parse(fs.readFileSync('./Data/UserData.json'))
 
-user.post('/users',)
+user.post('/users',(req,res)=>{
+    const NewId=data[data.length-1].id+1
+    const NewUser=Object.assign({id:NewId},req.body);
+    data.push(NewUser)
+
+    fs.writeFile('./Data/UserData.json',JSON.stringify(data),(err)=>err?res.status(500).json({status:'error',message:"Inernal Server Eroor"}):res.status(200).json({status:'succes',data:{user:NewUser}}))
+})
 user.get('/users',(req,res)=>{
     res.status(200).json({
         status:"success",
@@ -16,7 +23,77 @@ user.get('/users',(req,res)=>{
     })
 })
 user.get('/users/:id',(req,res)=>{
-    const newId=user
+    const Id=+req.params.id;
+    const user=data.find((id)=>id.id===Id);
+    if(!user){
+        res.status(404).json({
+            status:'error',
+            messge:'User Not Found !'
+        });
+        return
+    }
+    res.status(200).json({
+        status:'Succes',
+        data:{
+            user:user
+        },
+    });
+});
+user.patch('/users/:id',(req,res)=>{
+    const Id=+req.params.id
+    let updateUser=data.find((id)=>id.id===Id)
+
+    if(!updateUser){
+        res.status(404).json({
+            status:"error",
+            message:'User Not Found !'
+        });
+        return 
+    }
+    Object.assign(updateUser,req.body)
+    fs.writeFile('./Data/UserData.json',JSON.stringify(data),(err)=>{
+        if(err){
+            res.status(500).json({
+                status:"error",
+                message:'Internal Server Error'
+            });
+        }else{
+            res.status(200).json({
+                status:'succes',
+                data:{
+                    user:updateUser
+                },
+            });
+        }
+    });
 })
-user.put('/users/:id')
-user.delete('/users/:id')
+user.delete('/users/:id',(req,res)=>{
+    const Id=+req.params.id;
+    const delUser=data.find((id)=>id.id===Id);
+
+    if(!delUser){
+        res.status(404).json({
+            status:'Error',
+            message:'User Not Found !'
+        })
+        return;
+    }
+    const index=data.indexOf(delUser);
+    data.splice(index,1);
+    fs.writeFile('./Data/UserData.json',JSON.stringify(data),(err)=>{
+        if(err){
+            res.status(500).json({
+                status:'error',
+                message:'Internal Server Error'
+            });
+
+        }else{
+            res.status(200).json({
+                status:'succes',
+                data:{
+                    user:null
+                }
+            })
+        }
+    })
+})
